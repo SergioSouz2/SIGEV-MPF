0826e7d8b9ba8cdede24786dee011db281173215
Sergio Souz2 <sergio.tbl0123@gmail.com>
2025-10-27 11:05:00 -0300

    Initial commit

---
+++ src/main/java/com/sergio/SIGEV_MPF/SigevMpfApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.SIGEV_MPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigevMpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigevMpfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergio/SIGEV_MPF/SigevMpfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.SIGEV_MPF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigevMpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
